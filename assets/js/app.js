67b7c787ce233653eae81160fbc9ce96d6a03f57
Agamemnon94 <94798272+agamemnon94@users.noreply.github.com>
2022-06-30 17:48:51 +0200

    Update de app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,6 @@
-window.addEventListener('load', () => {
-  document.body.classList.remove("clean__transition");
-})
+// window.addEventListener('load', () => {
+//   document.body.classList.remove("clean__transition");
+// })
 
 // function compterMot() {
 //   var mots = document.getElementById("textarea").value;
@@ -14,32 +14,47 @@ window.addEventListener('load', () => {
 //   document.getElementById("afficher").innerHTML = nombre;
 // } 
 let countryBox = document.querySelector('.country_box');
-let box = document.createElement('div');
-box.className = 'boxe';
+
+// function countriesLanguages() {
+//   fetch('https://restcountries.com/v3.1/all')
+//     .then(response => response.json())
+//     .then(response => {
+//       response.forEach(element => {
+//         if (element.languages.fra === 'French') {
+//           let box = document.createElement('div');
+//           box.className = 'box';
+//           let flags = element.flags.png;
+//           let langue = element.languages.fra;
+//           let pays = element.name.common;
+//           box.style.backgroundImage = `url("${flags}")`
+//           countryBox.appendChild(box);
+//           box.innerHTML += `<h3>${pays}</h3>`;
+//           box.innerHTML += `<h4>${langue}</h4>`;
+//         }
+//       })
+//     })
+//     .catch(err => console.error(err));
+// };
+// countriesLanguages();
+
 
 function countriesLanguages() {
 
   fetch('https://restcountries.com/v3.1/all')
     .then(response => response.json())
     .then(response => {
-
-      // console.log(response);
-
       response.forEach(element => {
-
         if (element.hasOwnProperty('languages')) {
-
           if (element.languages.hasOwnProperty('fra')) {
+            let box = document.createElement('div');
+            box.className = 'box';
             let langue = element.languages.fra;
             let pays = element.name.common;
             let flags = element.flags.png;
-
             countryBox.appendChild(box);
-
+            box.style.backgroundImage = `url("${flags}")`
             box.innerHTML += `<h3>${pays}</h3>`;
             box.innerHTML += `<h4>${langue}</h4>`;
-            box.innerHTML += `<img src="${flags}" >`
-
           }
         }
       })
@@ -49,10 +64,3 @@ function countriesLanguages() {
 }
 countriesLanguages()
 
-
-
-
-// let arr = Object.keys(element).map(function (key) {
-//   return [Number(key), element[key]];
-// })
-// console.log(arr);
